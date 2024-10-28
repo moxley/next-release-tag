b524dc8a13cc09ef1b6f15579875e53f3650fa0d
Moxley Stratton <moxley.stratton@gmail.com>
2024-10-28 12:50:47 -0700

    Fix previous release tag

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
+import { setFailed, setOutput } from "@actions/core";
 import { fetchLatestReleaseTag } from "./services/githubService";
 import { calculateNewReleaseTag } from "./services/releaseService";
 
 const generateNextReleaseTag = async (): Promise<void> => {
   try {
-    const previousTagOverride = getInput("previous_tag");
-
-    const oldReleaseTag =
-      previousTagOverride || (await fetchLatestReleaseTag());
+    const oldReleaseTag = await fetchLatestReleaseTag();
     const newReleaseTag = calculateNewReleaseTag(oldReleaseTag);
 
     console.log(`Previous Release Tag: ${oldReleaseTag}`);
